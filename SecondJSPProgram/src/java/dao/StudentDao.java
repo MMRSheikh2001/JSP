f68a3780d96b2be0b47bb8e8f545dd17c78aa994
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-04-27 17:41:15 +0600

    hg

--- SecondJSPProgram/src/java/dao/StudentDao.java
+++ SecondJSPProgram/src/java/dao/StudentDao.java
@@ -7,6 +7,8 @@ package dao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.Student;
@@ -39,6 +41,82 @@ public class StudentDao {
             Logger.getLogger(StudentDao.class.getName()).log(Level.SEVERE, null, ex);
         }
         return value;
+    }
+
+    public static List<Student> findAll() {
+        List<Student> sList = new ArrayList<>();
+        sql = "select * from student1";
+        try {
+            ps = DbUtil.getCon().prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Student s = new Student(rs.getInt("id"), rs.getString("name"),
+                        rs.getString("email"), rs.getString("dob"), rs.getDouble("fee"));
+                sList.add(s);
+
+            }
+            rs.close();
+            ps.close();
+            DbUtil.getCon().close();
+        } catch (SQLException ex) {
+            Logger.getLogger(StudentDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return sList;
+    }
+
+    public static void delete(int id) {
+        sql = "delete from student1 where id=?";
+        try {
+            ps = DbUtil.getCon().prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.executeUpdate();
+            ps.close();
+            DbUtil.getCon().close();
+        } catch (SQLException ex) {
+            Logger.getLogger(StudentDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public static void update(Student s) {
+        sql = "update student1 set name=?,email=?,dob=?,fee=? where id=?";
+        try {
+            ps = DbUtil.getCon().prepareStatement(sql);
+            ps.setString(1, s.getName());
+            ps.setString(2, s.getEmail());
+            ps.setString(3, s.getDob());
+            ps.setDouble(4, s.getFee());
+            ps.setInt(5, s.getId());
+
+            ps.executeUpdate();
+            ps.close();
+            DbUtil.getCon().close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(StudentDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static Student findById(int id) {
+        Student s = null;
+        sql = "select * from student1 where id=?";
+        try {
+            ps = DbUtil.getCon().prepareStatement(sql);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                s = new Student(rs.getInt("id"), rs.getString("name"),
+                        rs.getString("email"), rs.getString("dob"), rs.getDouble("fee"));
+
+            }
+            rs.close();
+            ps.close();
+            DbUtil.getCon().close();
+        } catch (SQLException ex) {
+            Logger.getLogger(StudentDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return s;
     }
 
 }
