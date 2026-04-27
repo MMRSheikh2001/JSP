0b3582d48e744099f8db2296e15e9e642c388ae5
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-04-27 17:16:14 +0600

    gjh

---
+++ SecondJSPProgram/src/java/dao/StudentDao.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Student;
+import util.DbUtil;
+
+/**
+ *
+ * @author Admin
+ */
+public class StudentDao {
+
+    static PreparedStatement ps;
+    static ResultSet rs;
+    static String sql;
+
+    public static int save(Student s) {
+        int value = 0;
+        sql = "insert into student1 (name,email,dob,fee) values(?,?,?,?)";
+        try {
+            ps = DbUtil.getCon().prepareStatement(sql);
+            ps.setString(1, s.getName());
+            ps.setString(2, s.getEmail());
+            ps.setString(3, s.getDob());
+            ps.setDouble(4, s.getFee());
+
+            value = ps.executeUpdate();
+            ps.close();
+            DbUtil.getCon().close();
+        } catch (SQLException ex) {
+            Logger.getLogger(StudentDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return value;
+    }
+
+}
