5acdc3cc3136390e763707e01f5322d759d57587
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-05-01 21:26:09 +0600

    fhfh

--- custom/src/java/servlet/CustomServlet.java
+++ custom/src/java/servlet/CustomServlet.java
@@ -19,7 +19,7 @@ import java.io.IOException;
  */
 @WebServlet("customservlet")
 public class CustomServlet extends HttpServlet {
-
+    
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String action = req.getParameter("action");
@@ -33,20 +33,32 @@ public class CustomServlet extends HttpServlet {
             c.setCustom5(req.getParameter("custom5"));
             CustomDao.save(c);
             resp.sendRedirect("index.jsp");
-
+            
         } else if ("update".equalsIgnoreCase(action)) {
-
+            Custom c = new Custom();
+            c.setName(req.getParameter("name"));
+            c.setCustom1(req.getParameter("custom1"));
+            c.setCustom2(req.getParameter("custom2"));
+            c.setCustom3(req.getParameter("custom3"));
+            c.setCustom4(req.getParameter("custom4"));
+            c.setCustom5(req.getParameter("custom5"));
+            c.setId(Integer.parseInt(req.getParameter("id")));
+            CustomDao.update(c);
+            resp.sendRedirect("index.jsp");
         }
     }
-
+    
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String action = req.getParameter("action");
         if ("delete".equalsIgnoreCase(action)) {
-
+            CustomDao.delete(Integer.parseInt(req.getParameter("id")));
+            resp.sendRedirect("index.jsp");
         } else if ("edit".equalsIgnoreCase(action)) {
-
+            Custom c = CustomDao.getById(Integer.parseInt("id"));
+            req.setAttribute("cus", c);
+            req.getRequestDispatcher("edit.jsp").forward(req, resp);
         }
     }
-
+    
 }
---
+++ custom/web/edit.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : edit
+    Created on : May 1, 2026, 9:25:52 PM
+    Author     : Administrator
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
